fcb9cd2d6c23b8caf73b3afa4d6ef5ebc0428ad4
Benjamin Krall <ben@brandcoders.com>
2020-01-24 20:34:48 -0500

    add colors and theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import App from 'next/app'
+import Head from 'next/head'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import Typography from 'typography'
+import altonTheme from 'typography-theme-alton'
+
+const typography = new Typography(altonTheme)
 
 const theme = {
   colors: {
-    primary: '#0070f3',
-  },
+    color1: '#464159',
+    color2: '#6c7b95',
+    color3: '#8bbabb',
+    color4: '#c7f0db'
+  }
 }
 
 export default class MyApp extends App {
@@ -13,6 +21,10 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
+        <Head>
+          <link href="https://fonts.googleapis.com/css?family=Domine:400,700|Open+Sans:400,600,700&display=swap" rel="stylesheet" />
+          <style>{typography.toString()}</style>
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     )
